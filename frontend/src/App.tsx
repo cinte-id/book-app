7bcdd19b2b878f27a9bcfa8632a1f3ab44025510
Alfiyyah Nur Rachmania <nralfiyyahrach@gmail.com>
2026-04-27 10:25:51 +0700

    feat(App): register /books/:id route for BookDetail page
    - Import BookDetail component
    - Add <Route path='/books/:id' element={<BookDetail />} />
    - Placed above catch-all '*' route

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,7 +17,7 @@ const App = () => (
             <BrowserRouter>
                 <Routes>
                     <Route path="/" element={<Index />} />
-                    {/* Book detail page */}
+                    {/* Book Detail Page */}
                     <Route path="/books/:id" element={<BookDetail />} />
                     {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
                     <Route path="*" element={<NotFound />} />
